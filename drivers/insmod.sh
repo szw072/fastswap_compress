05298252d3c541402a7313ff094c54e5f0dcb628
zwsong <1072860370@qq.com>
2024-04-14 08:28:37 +0800

    添加sh

---
+++ drivers/insmod.sh
@@ -0,0 +1,8 @@
+echo -e "before\n"
+
+lsmod | grep fast
+sudo insmod fastswap_rdma.ko sport=50000 sip=10.10.10.9 cip=10.10.10.10 nc=20
+sudo insmod fastswap.ko
+
+echo -e "after\n"
+lsmod | grep fast
